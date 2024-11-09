f8a7475276d6def369d880ce1f69730ff61f4a0b
Gabriel <gcs232005@gmail.com>
2024-11-09 13:21:43 -0300

    Implementação myLib e Atividade

---
+++ C/ordenacao&analiseDeTemperatura.c
@@ -0,0 +1,24 @@
+/*                  Gabriel Carvalho De Souza 09 / 11 / 2024
+                        Ordenação E Análise De Temperatura                                   */
+
+/* Bibliotecas Auxiliares */
+#include <stdio.h>
+#include <stdlib.h>
+/* Fim Bibliotecas Auxiliares */
+
+
+/* Função Principal */
+int main ()
+{
+
+}
+/* Fim Função Principal */
+
+
+// Algoritmo 
+/*
+    - Ordenar uma lista em ordem crescente 
+    - Calcular a diferença entre a maior e a menor
+    - Encontrar a media das temperaturas
+    - Exibir a temperatura que mais repete
+*/
