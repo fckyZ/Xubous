f7e2a986dd0fd34276cdcb208bc314c22e5e35cb
Gabriel <gcs232005@gmail.com>
2024-11-17 21:14:31 -0300

    Atividade Implementada

--- C/prefixosComuns.c
+++ C/prefixosComuns.c
@@ -2,60 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-// ver o valor atual, ver o proximo valor
-// valor atual > prox  || soma += valor atual ? soma -= valor atual 
-// somar os valores
-
-typedef
-struct s_mapa_romano {
-    char simbolos [ 7 ];
-    int valores [ 7 ];
-} M;
-
-int romanToInt ( char * s )
+char * longestCommonPrefix ( char * * strs, int strsSize )
 {
-    M t = { { 'I', 'V', 'X', 'L', 'C', 'D', 'M' }, { 1, 5, 10, 50, 100, 500, 1000 } }; 
-    int soma = 0;
-    int lenWord = strlen ( s );
-
-    // valor atual
-    // valores de s, comparar com a lista da struct
-    for ( int j = 0; j < lenWord; j++ )
+    char * p = strs [ 0 ];
+    
+    for ( int i = 1; i < strsSize; i++ )
     {
-        int a = 0, p = 0;
-        for ( int i = 0; i < 7; i++ )
+        int j = 0;
+        while ( p [ j ] != '\0' && strs [ i ] [ j ] != '\0' && p [ j ] == strs [ i ] [ j ] )
         {
-            if ( s [ j ] == t.simbolos [ i ] )
-            {
-                a = t.valores [ i ];
-            }
+            j++;
         }
-
-        if ( j + 1 < lenWord )
-        {
-            for ( int i = 0; i < 7; i++ )
-            {
-                if ( s [ j + 1 ] == t.simbolos [ i ] )
-                {
-                    p = t.valores [ i ];
-                }
-            }
-        }
-        if ( a < p )
-        {
-            soma -= a;
-        } else
-            {
-                soma += a;
-            }
+        p[j] = '\0';
     }
-
-    return soma;
+    return p;
 }
 
 int main ()
 {
-    char * numeroRomano = "V";
-    printf ( "%d\n", romanToInt ( numeroRomano ) );
+    //
     return 0;
 }
